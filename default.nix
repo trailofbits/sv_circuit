8c4f6a3aad9180ef6ce67b30367ef6debef079ef
jleightcap <jack@leightcap.com>
2023-01-04 10:00:32 -0500

    TASK(jl): python nix hooks
    Signed-off-by: jleightcap <jack@leightcap.com>

--- default.nix
+++ default.nix
@@ -1,5 +1,12 @@
 let
   rust_overlay = import (builtins.fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz");
+
+  pkgs = import (builtins.fetchTarball {
+    name = "nixpkgs-unstable-2022-07-19";
+    url = "https://github.com/nixos/nixpkgs/archive/2df37941652c28e0858b9a9520ce5763c43c2ec1.tar.gz";
+    sha256 = "sha256:12d5w1bvhjlxrvdhsc44gq1lv5s3z1lv18s39q1702hwmp2bz071";
+  }) { overlays = [ rust_overlay ]; };
+
   rustVersion = "2022-10-01";
   rust = pkgs.rust-bin.nightly.${rustVersion}.default.override {
     extensions = [
@@ -9,11 +16,8 @@ let
     ];
   };
 
-  pkgs = import (builtins.fetchTarball {
-    name = "nixpkgs-unstable-2022-07-19";
-    url = "https://github.com/nixos/nixpkgs/archive/2df37941652c28e0858b9a9520ce5763c43c2ec1.tar.gz";
-    sha256 = "sha256:12d5w1bvhjlxrvdhsc44gq1lv5s3z1lv18s39q1702hwmp2bz071";
-  }) { overlays = [ rust_overlay ]; };
+  sv-python = pkgs.python311.withPackages (p: with p; [
+  ]);
 
 in
 with pkgs; stdenv.mkDerivation {
@@ -24,5 +28,6 @@ with pkgs; stdenv.mkDerivation {
     cacert
     git
     rust
+    sv-python
   ];
 }
