d69f94a51f4ec7852e22401d3b3ad23f97c4745f
jleightcap <jack@leightcap.com>
2023-02-05 18:02:56 -0500

    TASK(jl): `niv` pinned dependencies.
    Signed-off-by: jleightcap <jack@leightcap.com>

--- default.nix
+++ default.nix
@@ -1,5 +1,11 @@
+{ sources ? import ./nix/sources.nix
+, pkgs ? import sources.nixpkgs {
+    overlays = [
+      (import (fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz"))
+    ];
+  }
+}:
 let
-  rust_overlay = import (builtins.fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz");
   rustVersion = "2023-01-01";
   rust = pkgs.rust-bin.nightly.${rustVersion}.default.override {
     extensions = [
@@ -8,13 +14,6 @@ let
       "rustfmt"
     ];
   };
-
-  pkgs = import (builtins.fetchTarball {
-    name = "nixpkgs-unstable-2022-07-19";
-    url = "https://github.com/nixos/nixpkgs/archive/2df37941652c28e0858b9a9520ce5763c43c2ec1.tar.gz";
-    sha256 = "sha256:12d5w1bvhjlxrvdhsc44gq1lv5s3z1lv18s39q1702hwmp2bz071";
-  }) { overlays = [ rust_overlay ]; };
-
 in
 with pkgs; stdenv.mkDerivation {
   name = "sv_circuit";
