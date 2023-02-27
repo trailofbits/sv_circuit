a0f93fabc300431916dd54dda4fe31aadf66f22e
Jack Leightcap <jack.leightcap@trailofbits.com>
2023-02-27 17:56:16 -0500

    TEST(jl): development dependencies
    Signed-off-by: Jack Leightcap <jack.leightcap@trailofbits.com>

--- default.nix
+++ default.nix
@@ -1,5 +1,27 @@
 { sources ? import ./nix/sources.nix
-, pkgs ? import sources.nixpkgs { }
+, pkgs ? import sources.nixpkgs {
+    overlays = [
+      (import (fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz"))
+    ];
+  }
 }:
 
-pkgs.callPackage ./derivation.nix { }
+let
+  sv_circuit = pkgs.callPackage ./derivation.nix { };
+  rustVersion = "2023-01-01";
+  rust = pkgs.rust-bin.nightly.${rustVersion}.default.override {
+    extensions = [
+      "rust-src"
+      "clippy"
+      "rustfmt"
+    ];
+  };
+in
+with pkgs; stdenv.mkDerivation {
+  name = "sv_circuit";
+
+  nativeBuildInputs = [
+    git
+    rust
+  ];
+}
