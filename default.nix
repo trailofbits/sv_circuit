57649412c4b9243f48ecb6df17471b97973468d7
Jack Leightcap <jack.leightcap@trailofbits.com>
2023-02-27 23:57:06 -0500

    TEST(jl): use mozilla overlay
    Signed-off-by: Jack Leightcap <jack.leightcap@trailofbits.com>

--- default.nix
+++ default.nix
@@ -1,27 +1,5 @@
 { sources ? import ./nix/sources.nix
-, pkgs ? import sources.nixpkgs {
-    overlays = [
-      (import (fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz"))
-    ];
-  }
+, pkgs ? import sources.nixpkgs { }
 }:
 
-let
-  sv_circuit = pkgs.callPackage ./derivation.nix { };
-  rustVersion = "2023-01-01";
-  rust = pkgs.rust-bin.nightly.${rustVersion}.default.override {
-    extensions = [
-      "rust-src"
-      "clippy"
-      "rustfmt"
-    ];
-  };
-in
-with pkgs; stdenv.mkDerivation {
-  name = "sv_circuit";
-
-  nativeBuildInputs = [
-    git
-    rust
-  ];
-}
+pkgs.callPackage ./derivation.nix { }
--- derivation.nix
+++ derivation.nix
@@ -1,31 +1,24 @@
-{ lib
+{ sources ? import ./nix/sources.nix
 , rustPlatform
 }:
 
 let
-  sources = import ./nix/sources.nix;
-  pkgs = import sources.nixpkgs
-    {
-      overlays = [
-        (import (fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz"))
-      ];
-    };
-  rustVersion = "2023-01-01";
-  rust = pkgs.rust-bin.nightly.${rustVersion}.default.override {
-    extensions = [
-      "rust-src"
-      "clippy"
-      "rustfmt"
+  pkgs = import sources.nixpkgs {
+    overlays = [
+      (import (fetchTarball "https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz"))
     ];
   };
 in
-rustPlatform.buildRustPackage
-rec {
+with pkgs; rustPlatform.buildRustPackage rec {
   pname = "sv_circuit";
   version = "0.0.1";
 
   src = ./.;
 
-  nativeBuildInputs = [ rust ];
+  nativeBuildInputs = [
+    git
+    latest.rustChannels.nightly.rust
+  ];
+
   cargoHash = "sha256-Xmls6HqE7B2YQlZmiEfKRevb6vfaPnRY5r0bTiQyneI=";
 }
