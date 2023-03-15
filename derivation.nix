9184c26eb27b135fbd04de46bfcaa3b46773a67d
Jack Leightcap <jack.leightcap@trailofbits.com>
2023-03-15 16:53:53 -0400

    Auto-hash dependent crates, unblock dependabot PRs
    Signed-off-by: Jack Leightcap <jack.leightcap@trailofbits.com>

--- derivation.nix
+++ derivation.nix
@@ -1,24 +1,23 @@
-{ sources ? import ./nix/sources.nix
-, rustPlatform
-}:
+{ sources ? import ./nix/sources.nix, rustPlatform, lib }:
 
 let
   pkgs = import sources.nixpkgs {
     overlays = [
-      (import (fetchTarball "https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz"))
+      (import (fetchTarball
+        "https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz"))
     ];
   };
-in
-with pkgs; rustPlatform.buildRustPackage rec {
+in with pkgs;
+rustPlatform.buildRustPackage rec {
   pname = "sv_circuit";
   version = "0.0.1";
 
   src = ./.;
 
-  nativeBuildInputs = [
-    git
-    latest.rustChannels.nightly.rust
-  ];
+  nativeBuildInputs = [ git latest.rustChannels.nightly.rust ];
 
-  cargoHash = "sha256-Xmls6HqE7B2YQlZmiEfKRevb6vfaPnRY5r0bTiQyneI=";
+  cargoLock = {
+    lockFile = ./Cargo.lock;
+    allowBuiltinFetchGit = true;
+  };
 }
