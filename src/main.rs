47d41beba34bbd1f4813bf1ab37f62f4fc441db4
Jack Leightcap <jack.leightcap@trailofbits.com>
2023-09-20 19:43:15 -0400

    cli: use clap derive
    Signed-off-by: Jack Leightcap <jack.leightcap@trailofbits.com>

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 use std::fs::{read_to_string, File};
 use std::io::{BufReader, BufWriter};
-use std::path::Path;
+use std::path::{Path, PathBuf};
 
 use anyhow::{bail, Result};
-use clap::{arg, command};
+use clap::{command, Parser};
 use mcircuit::exporters::{Export, IR0};
-use mcircuit::parsers::blif::{BlifParser};
+use mcircuit::parsers::blif::BlifParser;
 use mcircuit::{Operation, Parse};
 
 use std::io;
@@ -26,55 +26,55 @@ fn emit_ir0(base_fname: &str, witness: &[bool]) -> Result<(), io::Error> {
     Ok(())
 }
 
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[clap(short, long, value_name = "BLIF")]
+    blif: PathBuf,
+
+    #[clap(short, long, value_name = "WITNESS")]
+    witness: PathBuf,
+
+    #[clap(short, long, value_name = "OUTPUT")]
+    output: PathBuf,
+}
+
 /// Rust version of circuit compositor
 fn main() -> Result<()> {
-    let matches = command!()
-        .arg(arg!(-b --boolean_circuit <FILE> "BLIF file"))
-        .arg(arg!(-w --witness <FILE> "Witness trace"))
-        .arg(arg!(-o --output_file <FILE> "Compiled circuit").required(true))
-        .get_matches();
+    let cli = Cli::parse();
 
-    let out_fname: &String = matches.get_one::<String>("output_file").unwrap(); // FIXME(jl): refactor clap
-                                                                                // types
-    let base_fname: &str = Path::new(out_fname)
+    // Parse and process input BLIF.
+    let blif = File::open(cli.blif)
+        .map(BufReader::new)
+        .map(BlifParser::<bool>::new)?;
+    let (flat, _, _) = sv_circuit::flatten(blif);
+
+    // Parse and process input witness.
+    // FIXME(jl): move this into a `parser` module.
+    let witness: Vec<bool> = read_to_string(cli.witness)?
+        .trim()
+        .chars()
+        .filter(|&c| c != '\n')
+        .flat_map(|c| match c {
+            '0' => Ok(false),
+            '1' => Ok(true),
+            _ => bail!("bad bit {:?} in witness!", c),
+        })
+        .collect();
+
+    let mut output = File::create(&cli.output).map(BufWriter::new)?;
+    let base_fname: &str = Path::new(&cli.output)
         .file_stem()
         .and_then(|s| s.to_str())
         .unwrap();
 
-    let maybe_bool = matches.get_one::<String>("boolean_circuit");
-    let maybe_witness = matches.get_one::<String>("witness");
-
-    match maybe_bool {
-        Some(path) => {
-            let reader = File::open(path).map(BufReader::new)?;
-            let (flat, _, _) = sv_circuit::flatten(BlifParser::<bool>::new(reader));
-
-            let mut writer = File::create(out_fname).map(BufWriter::new)?;
-
-            let witness = maybe_witness.expect("no witness for Bristol circuit!");
-            let w: Vec<bool> = read_to_string(witness)?
-                .trim()
-                .chars()
-                .filter(|&c| c != '\n')
-                .flat_map(|c| match c {
-                    '0' => Ok(false),
-                    '1' => Ok(true),
-                    _ => bail!("bad bit {:?} in witness!", c),
-                })
-                .collect();
-
-            // IR0
-            IR0::export_circuit(
-                &flat.into_iter().collect::<Vec<Operation<bool>>>(),
-                &w,
-                &mut writer,
-            )
-            .and(emit_ir0(base_fname, &w))?;
-        }
-        _ => {
-            println!("Usage: -a [arithmetic circuit file] -b [boolean circuit file] -c [connection circuit file] -w [witness] -o [output file]")
-        }
-    }
+    // IR0
+    IR0::export_circuit(
+        &flat.into_iter().collect::<Vec<Operation<bool>>>(),
+        &witness,
+        &mut output,
+    )?;
+    emit_ir0(base_fname, &witness)?;
 
     Ok(())
 }
